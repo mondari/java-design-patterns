0a73298f876191632fd6c120b46e2e300678c825
limengda <mondari@qq.com>
2022-03-08 21:14:25 +0800

    set ignorecase false

---
+++ decorator/src/main/java/com/mondari/Man.java
@@ -0,0 +1,11 @@
+package com.mondari;
+
+/**
+ * Man
+ */
+public class Man implements Person {
+
+    public void eat() {
+        System.out.println("a man is eating");
+    }
+}
---
+++ decorator/src/main/java/com/mondari/ManDecoratorA.java
@@ -0,0 +1,17 @@
+package com.mondari;
+
+/**
+ * ManDecoratorA
+ */
+public class ManDecoratorA extends Decorator {
+
+    public void eat() {
+        super.eat();
+        reEat();
+        System.out.println("ManDecoratorA Class");
+    }
+
+    public void reEat() {
+        System.out.println("eat another one");
+    }
+}
